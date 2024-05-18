bb97c8ba10634e673b1af86490b5b4e25e011fd1
egfs1 <eduardogomesfs4@gmail.com>
2024-05-18 14:18:24 -0300

    using hashSet instead of list to store VALIDATED_AUCTIONS and add new attributes 'level' and 'lock_level' to Item class

--- auctions.py
+++ auctions.py
@@ -4,15 +4,17 @@ import time
 import locale
 from items import items, Tier, get_color_code
 
-AUCTIONS_SHOWED = []
+VALIDATED_AUCTIONS = set()
 CONSOLE_DEFAULT_COLOR = "\033[0m"
 STAR_CHAR = "✪"
 locale.setlocale(locale.LC_ALL, 'de_DE')
 
 
 def validate_and_print_auction(auction):
-    if not auction["bin"] or auction["claimed"] or is_already_showed(auction):
+    if not auction["bin"] or auction["claimed"] or is_already_validated(auction):
         return
+    
+    VALIDATED_AUCTIONS.add(auction["uuid"])
 
     for item in items:
         if item.name not in auction["item_name"]:
@@ -27,8 +29,8 @@ def validate_and_print_auction(auction):
         print_auction(auction, get_color_code(Tier[auction["tier"]]))
 
 
-def is_already_showed(auction):
-    return auction["uuid"] in AUCTIONS_SHOWED
+def is_already_validated(auction):
+    return auction["uuid"] in VALIDATED_AUCTIONS
 
 
 def has_required_attributes(item, auction):
@@ -41,6 +43,15 @@ def has_required_attributes(item, auction):
     # Check if the item has the required tier
     if not check_item_attribute(item.tier.value, Tier[auction["tier"]].value, item.lock_tier):
         return False
+    
+    # Check if the item has the required level
+    if item.level > 0:
+        if "Lvl" in auction["item_name"]:
+            auction_level = int(auction["item_name"].split("]")[0].split("[Lvl ")[1])
+            if not check_item_attribute(item.level, auction_level, item.lock_level):
+                return False
+        else:
+            return False
     
     # Check if the item has the required price
     if item.price < int(auction["starting_bid"]):
@@ -72,6 +83,5 @@ def print_auction(auction, color_code):
     print(time.strftime('%H:%M:%S', time.localtime()))
     print("")
 
-    AUCTIONS_SHOWED.append(auction["uuid"])
     pyperclip.copy(f"viewauction {auction['uuid']}")
     winsound.Beep(1000, 1000)
--- items.py
+++ items.py
@@ -2,14 +2,16 @@ from enum import Enum
 
 
 class Item:
-    def __init__(self, name, stars, tier, price, enchantments=[], lock_stars=False, lock_tier=False):
+    def __init__(self, name, tier, price, stars=0, enchantments=[], level=0, lock_stars=False, lock_tier=False, lock_level=False):
         self.name = name
         self.stars = stars
         self.tier = tier
         self.price = price
         self.enchantments = enchantments
+        self.level = level
         self.lock_stars = lock_stars
         self.lock_tier = lock_tier
+        self.lock_level = lock_level
 
 
 class Tier(Enum):
@@ -50,6 +52,6 @@ def get_color_code(tier):
 
 
 items = [
-    Item(name = "Ancient Shadow Assassin Leggings", stars = 4, tier = Tier.LEGENDARY, price = 4500000),
-    Item(name = "Necrotic Wise Dragon Leg", stars = 0, tier = Tier.LEGENDARY, price = 1600000),
+    Item(name = "Ancient Shadow Assassin Chestplate", stars = 5, tier = Tier.MYTHIC, price = 20000000, enchantments=["Last Stand V"]),
+    Item(name = "Baby Yeti", level=90, tier = Tier.LEGENDARY, price = 30000000),
 ]
