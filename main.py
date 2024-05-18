fc63fdc3761eaf13b8d5e8e5f07a71b49cf288ad
egfs1 <eduardogomesfs4@gmail.com>
2024-05-18 14:15:33 -0300

    using two separated ThreadPoolExecutor. Both fetching of URLs and processing of auction data are concurrent and handled by separate executors

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import concurrent.futures
 from auctions import validate_and_print_auction
 
 
-API_URL = "https://api.hypixel.net/skyblock/auctions"
+API_URL = "https://api.hypixel.net/v2/skyblock/auctions"
 MOST_RECENT_PAGES = 2
 session = requests.Session()
-
 
 def run():
     print("Initializing...")
@@ -18,7 +17,7 @@ def run():
 
     # Fetch all pages of auctions
     fetch_pages(total_pages_count)
-    
+
     while True:
         # Fetch most recent pages of auctions
         fetch_pages(MOST_RECENT_PAGES)
@@ -30,15 +29,18 @@ def fetch_pages(page_count):
         for i in range(page_count):
             urls.append(f'{API_URL}?page={i}')
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(fetch_url, url) for url in urls]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as fetch_executor, concurrent.futures.ThreadPoolExecutor(max_workers=50) as process_executor:
+            fetch_futures = [fetch_executor.submit(fetch_url, url) for url in urls]
+            process_futures = []
 
-            for future in concurrent.futures.as_completed(futures):
+            for future in concurrent.futures.as_completed(fetch_futures):
                 result = future.result()
-                
                 if result is not None and result["success"]:
                     for auction in result["auctions"]:
-                        validate_and_print_auction(auction)
+                        process_futures.append(process_executor.submit(validate_and_print_auction, auction))
+
+            for future in concurrent.futures.as_completed(process_futures):
+                future.result()
 
     except KeyboardInterrupt:
         print("Program stopped by user, exiting...")
@@ -49,8 +51,8 @@ def fetch_url(url):
     try:
         response = session.get(url)
         return response.json()
-    except requests.exceptions.RequestException:
-        print(f"An error occurred while fetching the URL: {url}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching the URL: {url} = {e}")
         return None
 
 run()
